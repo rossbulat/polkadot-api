153cd7bc6c1853d51cdc1cec41427b1cb509b52b
peetzweg/ <phil.czek@gmail.com>
2023-11-08 15:32:16 +0100

    chore: removes vite starter project files

--- packages/react-viewer/src/main.tsx
+++ packages/react-viewer/src/main.tsx
@@ -1,10 +1,2 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
-import "./index.css"
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export { DecodedViewer } from "./DecodedViewer"
+export { CallDecodedViewer } from "./CallDecodedViewer"
